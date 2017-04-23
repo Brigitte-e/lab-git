4642c24e07c250902c3b64d37fc5a55a840d2aaf
Anastasiia Khymko <anastasiia.khymko@osf-global.com>
2017-04-23 16:16:08 +0300

    purse all

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -13,8 +13,11 @@ namespace Purse
             CreditCart.Class1 debet = new CreditCart.Class1();
             CreditCart.Class1 credet = new CreditCart.Class1();
 
+            int money = debet.debet + credet.credet;
+
             Console.WriteLine("Your debet"+debet.debet);
             Console.WriteLine("Your credet" + credet.credet);
+            Console.WriteLine("All" + money);
 
         }
     }
